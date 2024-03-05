701cdde4707cf8f462d35faac894589b3b007acf
Jongmin Lee <45500778+tltmzmaos@users.noreply.github.com>
2024-03-05 00:03:51 -0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -11,10 +11,12 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
+    /*
     {
       name: `morethan-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
+    */
   ],
   // blog setting (required)
   blog: {
