fa54c8b66710cd13cb40100058c6c2c45df925bd
Jongmin Lee <45500778+tltmzmaos@users.noreply.github.com>
2024-03-04 22:49:09 -0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "JM's blog",
+    description: "welcome to my blog!",
   },
 
   // CONFIG configration (required)
